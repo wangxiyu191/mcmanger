9714aa246511bbf468a0a4f5f41db15c4233d671
W <910741878@qq.com>
2015-06-18 18:25:47 +0800

    Minecraft监测完成

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"github.com/astaxie/beego/config"
+	//"github.com/go-martini/martini"
 	"mcmanger/mc"
+	"mcmanger/monitor"
 	"mcmanger/qc"
 )
 
@@ -13,8 +15,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	//fmt.Println("zone:" + conf.String("QingCloud::ServerId"))
-
 	qc.Init(
 		conf.String("QingCloud::Zone"),
 		conf.String("QingCloud::AccessKeyId"),
@@ -22,16 +22,21 @@ func main() {
 		conf.String("QingCloud::ServerId"),
 	)
 	mc.Init(conf.String("Minecraft::Address"))
+	/*
+		qcStatus, err := qc.Status()
+		fmt.Println("QingCloud Instances:" + qcStatus)
 
-	qcStatus, err := qc.Status()
-	fmt.Println("QingCloud Instances:" + qcStatus)
+		mcStatus, err := mc.Status()
+		fmt.Printf("%d player online.\n", mcStatus.Players.Online)
+		for _, player := range mcStatus.Players.Sample {
+			fmt.Println(player.ID + "  " + player.Name)
+		}
 
-	mcStatus, err := mc.Status()
-	fmt.Printf("%d player online.\n", mcStatus.Players.Online)
-	for _, player := range mcStatus.Players.Sample {
-		fmt.Println(player.ID + "  " + player.Name)
-	}
-
-	err = qc.Boot()
-	fmt.Println(err)
+		err = qc.Boot()
+		fmt.Println(err)
+	*/
+	monitor.Start(
+		conf.String("Monitor::FirstDuration"),
+		conf.String("Monitor::SecondDuration"),
+	)
 }
---
+++ monitor/monitor.go
@@ -0,0 +1,69 @@
+package monitor
+
+import (
+	"log"
+	"mcmanger/mc"
+	"mcmanger/qc"
+	"time"
+)
+
+func getPlayerNum() (playerNum int, err error) {
+	response, err := mc.Status()
+	if err != nil {
+		return 0, err
+	}
+	return response.Players.Online, nil
+}
+
+func parseDuration(durstr string) (duration time.Duration) {
+	duration, err := time.ParseDuration(durstr)
+	if err != nil {
+		panic(err)
+	}
+	return duration
+}
+
+func Start(first string, second string) {
+	firstDuration := parseDuration(first)
+	secondDuration := parseDuration(first)
+
+	timer := time.NewTimer(firstDuration)
+	nonePlayerTimes := 0 //已经连续几次检测到服务器没有人
+	for {
+		<-timer.C
+		timer.Reset(firstDuration) //循环监测
+
+		//服务器是否开机
+		qcStatus, err := qc.Status()
+		if err != nil {
+			log.Println("[Monitor]Error:" + err.Error())
+			continue
+		}
+		if qcStatus != "running" {
+			log.Println("[Monitor]Server is not Running.")
+			continue
+		}
+
+		playerNum, err := getPlayerNum()
+		if err != nil {
+			log.Println("[Monitor]Error:" + err.Error())
+			continue
+		}
+
+		if playerNum == 0 {
+			if nonePlayerTimes == 1 {
+				log.Println("[Monitor]第二次发现无人在线，停机")
+				qc.Stop()
+			} else {
+				log.Println("[Monitor]第一次发现无人在线，进入第二阶段监测")
+				nonePlayerTimes++
+				timer.Reset(secondDuration)
+			}
+		} else {
+			log.Printf("[Monitor]有%d人在线", playerNum)
+			nonePlayerTimes = 0
+		}
+
+	}
+
+}
